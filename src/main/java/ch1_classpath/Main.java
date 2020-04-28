28dbb5c0d5fab20430f7161ee7b68647548a7b2e
wuqingze <nongxiaolang@gmail.com>
2020-04-28 18:59:15 +0800

    maven子模块依赖父模块 打成自运行的jar包并包含依赖

---
+++ src/main/java/ch1_classpath/Main.java
@@ -0,0 +1,10 @@
+package ch1_classpath;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
